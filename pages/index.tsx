987c6dbc561ff81c4c86be407916f7a5e330bc49
Brandon Bloom <brandon@brandonbloom.name>
2021-07-16 16:52:38 -0700

    temporary ugly install instructions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,46 @@
 import Head from "next/head";
 
 export default function Home() {
-	return (
-		<div>
-			<Head>
-				<title>Exo</title>
-				<meta
-					name="description"
-					content={`A process manager & log viewer for dev`}
-				/>
-				<meta content="dark light" name="color-scheme" />
-				<link rel="icon" href="/deref-rounded-icon.png" />
-			</Head>
+  return (
+    <div>
+      <Head>
+        <title>exo</title>
+        <meta
+          name="description"
+          content={`A process manager & log viewer for dev`}
+        />
+        <meta content="dark light" name="color-scheme" />
+        <link rel="icon" href="/deref-rounded-icon.png" />
+      </Head>
 
-			<h1>Exo</h1>
-			<p>{`Process manager & log viewer for dev`}</p>
+      <h1>exo</h1>
+      <p>{`Process manager & log viewer for dev`}</p>
 
-			<br />
+      <br />
 
-			<h3>Install</h3>
-			<pre>{`$ curl -sL https://run.exo.deref.io/install | sh`}</pre>
+      <h3>Install</h3>
+      <pre>{`$ curl -sL https://download-page.deref.workers.dev/install.sh | sh`}</pre>
 
-			<hr />
+      <hr />
 
-			<h3>Getting started</h3>
-			<p>
-				Run <code>exo gui</code> in your project directory
-			</p>
+      <h3>Getting started</h3>
+      <p>
+        Add exo to your <code>PATH</code>:
+      </p>
+      <pre>{`$ export PATH="$PATH:$HOME/.exo/bin"`}</pre>
+      <p>
+        Run <code>exo gui</code> in your project directory.
+      </p>
 
-			<hr />
+      <hr />
 
-			<p>
-				<a href="https://github.com/deref/exo">Github repository</a>
-			</p>
+      <p>
+        <a href="https://github.com/deref/exo">Github repository</a>
+      </p>
 
-			<br />
+      <br />
 
-			<p>Copyright 2021 Deref Inc. &nbsp; All rights reserved.</p>
-		</div>
-	);
+      <p>Copyright 2021 Deref Inc. &nbsp; All rights reserved.</p>
+    </div>
+  );
 }
