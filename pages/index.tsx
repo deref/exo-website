fbdcc139badfca5a3c8737f104354785bdd4e89c
John <51100181+jwmza@users.noreply.github.com>
2021-07-17 23:13:06 -0400

    Add basic mockup image to landing page and improve site styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,60 @@
 import Head from "next/head";
 
+import SvgGUI from "~/public/svg/dark-exo.svg";
+
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>exo</title>
-        <meta
-          name="description"
-          content={`A process manager & log viewer for dev`}
-        />
-        <meta content="dark light" name="color-scheme" />
-        <link rel="icon" href="/deref-rounded-icon.png" />
-      </Head>
+	return (
+		<div>
+			<Head>
+				<title>exo</title>
+				<meta
+					name="description"
+					content={`A process manager & log viewer for dev`}
+				/>
+				<meta content="dark light" name="color-scheme" />
+				<link rel="icon" href="/deref-rounded-icon.png" />
+			</Head>
 
-      <h1>exo</h1>
-      <p>{`Process manager & log viewer for dev`}</p>
+			<h1>exo</h1>
+			<p>{`Process manager & log viewer for dev`}</p>
 
-      <br />
+			<br />
 
-      <h3>Install</h3>
-      <pre>{`$ curl -sL https://download-page.deref.workers.dev/install.sh | sh`}</pre>
-      <p>
-        <small>
-          Prefer manual installation? See{" "}
-          <a href="https://github.com/deref/exo/blob/main/doc/install.md">
-            install.md
-          </a>
-        </small>
-      </p>
+			<div className="svg-wrapper">
+				<SvgGUI />
+			</div>
 
-      <hr />
+			<br />
 
-      <h3>Getting started</h3>
-      <p>
-        Add exo to your <code>PATH</code>:
-      </p>
-      <pre>{`$ export PATH="$PATH:$HOME/.exo/bin"`}</pre>
-      <p>
-        Run <code>exo gui</code> in your project directory.
-      </p>
+			<h3>Install</h3>
+			<pre>{`curl -sL https://download-page.deref.workers.dev/install.sh | sh`}</pre>
+			<p>
+				Prefer manual installation? See{" "}
+				<a href="https://github.com/deref/exo/blob/main/doc/install.md">
+					install.md
+				</a>
+			</p>
 
-      <hr />
+			<hr />
 
-      <p>
-        <a href="https://github.com/deref/exo">Github repository</a>
-      </p>
+			<h3>Getting started</h3>
+			<p>
+				Add exo to your <code>PATH</code>:
+			</p>
+			<pre>{`export PATH="$PATH:$HOME/.exo/bin"`}</pre>
+			<p>
+				Run <code>exo gui</code> in your project directory.
+			</p>
 
-      <br />
+			<hr />
 
-      <p>Copyright 2021 Deref Inc. &nbsp; All rights reserved.</p>
-    </div>
-  );
+			<p>
+				<a href="https://github.com/deref/exo">Github repository</a>
+			</p>
+
+			<br />
+
+			<p>Copyright 2021 Deref Inc. &nbsp; All rights reserved.</p>
+		</div>
+	);
 }
